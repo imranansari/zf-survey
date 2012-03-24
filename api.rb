f8747ef68ddaa31fe2dd99d6f8a2f57aa2c15731
iansari <imran.iansari@gmail.com>
2012-03-24 17:16:42 -0400

    init

---
+++ api.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+require 'json'
+
+class API < Sinatra::Base
+  get '/sushi.json' do
+    content_type :json
+    
+    {:sushi => ["Maguro", "Hamachi", "Uni", "Saba", "Ebi", "Sake", "Tai"]}.to_json
+  end
+end
