00071cf99d51e55529353280c498a85be612c523
Imran Ansari <imran.iansari@gmail.com>
2013-04-26 02:05:32 -0300

    remove mongoid

--- api.rb
+++ api.rb
@@ -4,16 +4,7 @@ require 'mongoid'
 
 class API < Sinatra::Base
 
-  # MongoDB configuration
-  Mongoid.configure do |config|
-    if ENV['MONGOHQ_URL']
-      conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
-      uri = URI.parse(ENV['MONGOHQ_URL'])
-      config.master = conn.db(uri.path.gsub(/^\//, ''))
-    else
-      config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('test')
-    end
-  end
+
 
   # Models
   class Counter
